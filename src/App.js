972ceb1833be53ef5bdeef3661a73770e54bf965
Krisztian Holub <kholub1989@gmail.com>
2020-09-27 22:43:53 -0400

    Build the banner of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
+import Banner from "./Banner";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
